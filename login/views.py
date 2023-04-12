c0968a8828056d868be02a7fc0333d320b402e00
Ticketgateway2023 <ishwar@thoughtwin.com>
2023-04-12 15:43:46 +0530

    change home view

--- login/views.py
+++ login/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def methodinfo(request):  
-    return HttpResponse("poonam:"+request.method)  
+    return HttpResponse("Welcome Page:"+request.method)  
