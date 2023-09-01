d399e48f1fbf3ecf9d4d462ffed3d7e41d84a6a9
poonamk3 <113355298+poonamk3@users.noreply.github.com>
2023-09-01 17:33:02 +0530

    Update views.py

--- login/views.py
+++ login/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def methodinfo(request):  
-    return HttpResponse("indore:"+request.method)  
+    return HttpResponse("Python is an interpreted, object-oriented, high-level programming language with dynamic semantics developed by Guido van Rossum. It was originally released in 1991. Designed to be easy as well as fun, the name "Python" is a nod to the British comedy group Monty Python.:"+request.method)  
