885e781612f98b53ae262113a1617527a8a4774b
poonamk3 <poonamk@thoughtwin.com>
2023-09-04 14:04:21 +0530

    python opps:

--- login/views.py
+++ login/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def methodinfo(request):  
-    return HttpResponse("Welcome Indore.:"+request.method)  
+    return HttpResponse("python opps.:"+request.method)  
 
